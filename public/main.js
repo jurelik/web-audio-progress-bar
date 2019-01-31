c3405f76514fa217cd4ab32628fbbaf002e79d72
jurelik <jure@likar.me>
2019-01-31 19:25:14 +0000

    progress bar is functional

--- public/main.js
+++ public/main.js
@@ -1,6 +1,8 @@
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 const context = new AudioContext();
+
 let buffer;
+const progressBar = document.getElementById('progress-bar');
 
 fetch('warble.mp3').then(res => {
   return res.arrayBuffer();
@@ -8,6 +10,7 @@ fetch('warble.mp3').then(res => {
   context.decodeAudioData(res).then(decoded => {
     buffer = decoded;
     console.log('decoded');
+    console.log(buffer);
   }).catch(error => {
     console.log(error);
   });
@@ -17,7 +20,23 @@ fetch('warble.mp3').then(res => {
 
 function play() {
   let source = context.createBufferSource();
+  let startTime = context.currentTime;
+
   source.buffer = buffer;
   source.connect(context.destination);
   source.start();
+
+  updateProgress(startTime);
+}
+
+function updateProgress(startTime) {
+  let length = buffer.duration;
+
+  setTimeout(() => {
+    let progress = (context.currentTime - startTime) / length * 100;
+    progressBar.value = progress;
+
+    console.log(progressBar.value);
+    updateProgress(startTime);
+  }, 500);
 }
